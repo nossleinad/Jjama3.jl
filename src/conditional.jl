01917e244f8494b5ee38b97e6f0f0edad39881cd
nossleinad <maximilian.danielsson@gmail.com>
2025-07-07 10:29:17 +0200

    Enable sampling from ConditionalTransformer

--- src/conditional.jl
+++ src/conditional.jl
@@ -102,3 +102,84 @@ end
 # compat
 forward_inference(model, args...) = model(args...)
 forward_loss(model::ConditionalTransformer, inputs::AbstractArray, conditionals, targets::AbstractArray; clear_cache = true, loss_mask = nothing) = loss(model(inputs, conditionals, clear_cache = clear_cache), targets, loss_mask = loss_mask) 
+
+### Sampling ###
+
+function nexttoken!(tokens, model, sampler, logits, tokenizer_for_printing)
+    tokens[model.pos+1] = sampler(logits[:, end, 1])
+    !isnothing(tokenizer_for_printing) && print(decode(tokenizer_for_printing, [tokens[model.pos+1]], skip_special_tokens = false))
+end
+
+"""
+    generate(model, initial_tokens; max_new_tokens=100, sampler=top_pk_sampler(p=0.5f0, k=5), tokenizer_for_printing=tkn, end_token=128010)
+
+Takes an initial sequence of tokens, and generates new tokens one at a time until the end token is sampled. Uses a KV cache. No batch dim for now.
+Runs on CPU by default. If the model is on the GPU (assuming Flux.jl, eg. `model = gpu(model)`), then pass `device = gpu` to `generate` to run on the GPU.
+
+```julia
+tkn = llama3_tokenizer()
+generate(model, initial_tokens; max_new_tokens=100, sampler=top_pk_sampler(p=0.5f0, k=5), tokenizer_for_printing=tkn, end_token=128010)
+```
+"""
+function generate(
+    model::ConditionalTransformer, 
+    initial_tokens::AbstractArray{<:Integer},
+    conditionals::Tuple;
+    max_new_tokens=100,
+    sampler::Function=argmax_sampler,
+    tokenizer_for_printing = nothing,
+    end_token = 128010,
+    clear_cache = true,
+    pos_offset = 0,
+    device = identity,
+    sdpa_func = sdpa
+)
+    tokens = vcat(initial_tokens, similar(initial_tokens, max_new_tokens))
+    if clear_cache
+        clear_cache!(model)
+        config_cache!(model, length(initial_tokens) + max_new_tokens)
+    else
+        extend_cache!(model, length(initial_tokens) + max_new_tokens)
+    end
+    input_tokens = device(reshape(initial_tokens, :, 1))  # (seq_len, batch=1)
+    logits = model(input_tokens, conditionals, sdpa_func = sdpa_func)
+    if max_new_tokens > 0
+        nexttoken!(tokens, model, sampler, logits, tokenizer_for_printing)
+        tokens[model.pos+1] == end_token && return tokens[1:model.pos+1]
+    else
+        return tokens
+    end
+    for _ in 1:max_new_tokens-1
+        input_tokens = device(reshape([tokens[model.pos+1]], :, 1))  # Just the last token
+        logits = model(input_tokens, conditionals, sdpa_func = sdpa_func)
+        nexttoken!(tokens, model, sampler, logits, tokenizer_for_printing)
+        tokens[model.pos+1] == end_token && break
+    end
+    return tokens[1:model.pos+1]
+end
+
+generate(
+    model::ConditionalTransformer, 
+    initial_tokens::AbstractArray{<:Integer},
+    conditional;
+    max_new_tokens=100,
+    sampler::Function=argmax_sampler,
+    tokenizer_for_printing = nothing,
+    end_token = 128010,
+    clear_cache = true,
+    pos_offset = 0,
+    device = identity,
+    sdpa_func = sdpa
+) = generate(
+    model, 
+    initial_tokens, 
+    (conditional,);
+    max_new_tokens, 
+    sampler, 
+    tokenizer_for_printing, 
+    end_token, 
+    clear_cache, 
+    pos_offset, 
+    device, 
+    sdpa_func
+)
